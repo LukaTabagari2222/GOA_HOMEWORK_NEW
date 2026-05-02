6da7a0ce92d0455d7ff1bf90805e7977f44eb99b
LukaTabagari2222 <tabagariluka32@gmail.com>
2026-05-02 18:57:58 +0400

    commit

---
+++ GAK_06_goa/index.js
@@ -0,0 +1,81 @@
+//   DAV N1
+//    const numbers = [1, 2, 3, 4, 5]
+//    numbers.forEach(num => {
+//     console.log(num)
+//    })
+
+// DAV N2
+//    const names = ["John", "Sarah", "Mike"];
+
+//    names.forEach(num => {
+//      console.log("HELLO: " + num)
+//    })
+
+// DAV N3
+// let zero=0
+//     const numbers=[1,2,3,4,5,6,7,8,9,10]
+//     numbers.forEach(numbers=>{
+//         zero+=numbers
+//         console.log(zero)
+//     })
+
+// DAV N4
+//   const words = ["javascript", "is", "awesome"];
+// words.forEach(element => {
+//    console.log(`${element.length} ${element}`)
+// });
+
+// DAV N5
+// const students=["luka","davit","girogi","lasha"]
+// let x=-1
+// students.forEach(nexstu =>{
+//     x+=1
+//     console.log(x +"-"+ nexstu )
+// })
+
+// DAV N6
+// const numersMasiv=[1,2,3,4,5,55,133,145]
+
+// let returnsed=numersMasiv.map(num=>{
+//    console.log( num *2)
+// })
+
+//  DAV N7
+
+// const temperture=[1,2,3,4,5,10,15,20,25]
+// let bigtemp=temperture.map( num=> num  * 9/5 + 32)
+// console.log(bigtemp)
+
+// DAV N8
+// const nameMsiv = ["luka", "toma", "mate"];
+// const nextupperMasiv = nameMsiv.map((names) => {
+//   console.log(names.toUpperCase());
+// });
+
+
+// DAV N9
+//  const users = [
+//      { name: "John", age: 20 },
+//      { name: "Sarah", age: 25 }
+//    ];
+
+//    const nextUsers=users.map(namee=>{
+//     if( namee.name === "John"){
+//         return { ...namee, name: "saba"}
+
+//     }
+//     if( namee.name === "Sarah"){
+//         return {... namee ,name: "daviti"}
+//     }
+//     return namee
+//    })
+//    console.log(nextUsers)
+
+
+
+// DAV N10
+// const lastnumBers=[1,2,3,4,5,6,7,8,9,10]
+//  const nextmAsiv=lastnumBers.map(numbernex =>{
+//       let struing = "Number " +numbernex
+//        console.log(struing)
+//  })
